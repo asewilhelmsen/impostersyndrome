dd6c5e8fc10265dc58bcf31ff34b84758bf073b4
elisetelje <teljeelise@gmail.com>
2023-12-11 22:15:52 +0100

    oppdatert kode for kobling til database

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,26 @@
 import Test from "../components/Test";
+import { FormEvent, useRef } from "react";
+import handleSubmit from "../firebase/handles/handleSubmit";
 
 const Login = () => {
+  //Test for å skrive til database
+  const dataRef = useRef<HTMLInputElement>(null);
+
+  const submithandler = (e: FormEvent) => {
+    e.preventDefault();
+    if (dataRef.current) {
+      handleSubmit(dataRef.current.value);
+      dataRef.current.value = "";
+    }
+  };
   return (
     <>
       <div>Start siden med join team knapp</div>
       <Test message="Dette er en test" />
+      <form onSubmit={submithandler}>
+        <input type="text" ref={dataRef} />
+        <button type="submit">Save</button>
+      </form>
     </>
   );
 };
