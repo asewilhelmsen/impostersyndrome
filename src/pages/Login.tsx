132608cc4ccf2f036f9b5f4e5eddcb692ab8f680
elisetelje <teljeelise@gmail.com>
2023-12-11 22:32:59 +0100

    skal hente ut data for teams fra firebase

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,13 @@
 import Test from "../components/Test";
-import { FormEvent, useRef } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import handleSubmit from "../firebase/handles/handleSubmit";
+import { collection, getDocs } from "@firebase/firestore";
+import { firestore } from "../firebase/firebase_setup/firebase";
 
+interface Teams {
+  id: string;
+  // other fields...
+}
 const Login = () => {
   //Test for å skrive til database
   const dataRef = useRef<HTMLInputElement>(null);
@@ -13,6 +19,25 @@ const Login = () => {
       dataRef.current.value = "";
     }
   };
+
+  //Test for å lese fra database
+  const [data, setData] = useState<Teams[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(firestore, "teams"));
+
+      const newData: Teams[] = [];
+      querySnapshot.forEach((doc) => {
+        newData.push({ id: doc.id, ...doc.data() } as Teams);
+      });
+
+      setData(newData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div>Start siden med join team knapp</div>
@@ -21,6 +46,14 @@ const Login = () => {
         <input type="text" ref={dataRef} />
         <button type="submit">Save</button>
       </form>
+      <div>
+        <h2>Data from Firestore:</h2>
+        <ul>
+          {data.map((item: Teams) => (
+            <li key={item.id}>{JSON.stringify(item)}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
