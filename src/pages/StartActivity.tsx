bbd0abab018d5da2e91d458fc2e885c5d4d9ba8a
amw <ase.wilhelmsen@outlook.com>
2023-12-18 16:37:09 +0100

    gjøre stepper til generell komponent

--- src/pages/StartActivity.tsx
+++ src/pages/StartActivity.tsx
@@ -1,10 +1,22 @@
 import Steps from "../components/Stepper";
 
+const Step1Content = () => <div>This is the content for Step 1</div>;
+const Step2Content = () => <div>This is the content for Step 2</div>;
+const Step3Content = () => <div>This is the content for Step 3</div>;
+const Step4Content = () => <div>This is the content for Step 4</div>;
+
 const StartActivity = () => {
+  const steps = ["Icebreaker", "Learn about", "Conversation", "Expectations"];
+  const stepComponents = [
+    <Step1Content />,
+    <Step2Content />,
+    <Step3Content />,
+    <Step4Content />,
+  ];
+
   return (
     <div>
-      <div>Første side på start aktiviteten</div>
-      <Steps></Steps>
+      <Steps nameList={steps} content={stepComponents} />
     </div>
   );
 };
