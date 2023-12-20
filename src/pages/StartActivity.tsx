c86463f56b1027a6afb9ed0dab691528a3a8efc4
asewilhelmsen <101637785+asewilhelmsen@users.noreply.github.com>
2023-12-20 22:46:19 +0100

    #5 create stepper (#14)
    * lagt til stepper for ulike steg
    
    * gjøre stepper til generell komponent
    
    * gitignore
    
    ---------
    
    Co-authored-by: amw <ase.wilhelmsen@outlook.com>

--- src/pages/StartActivity.tsx
+++ src/pages/StartActivity.tsx
@@ -1,5 +1,24 @@
+import Steps from "../components/Stepper";
+
+const Step1Content = () => <div>This is the content for Step 1</div>;
+const Step2Content = () => <div>This is the content for Step 2</div>;
+const Step3Content = () => <div>This is the content for Step 3</div>;
+const Step4Content = () => <div>This is the content for Step 4</div>;
+
 const StartActivity = () => {
-  return <div>Første side på start aktiviteten</div>;
+  const steps = ["Icebreaker", "Learn about", "Conversation", "Expectations"];
+  const stepComponents = [
+    <Step1Content />,
+    <Step2Content />,
+    <Step3Content />,
+    <Step4Content />,
+  ];
+
+  return (
+    <div>
+      <Steps nameList={steps} content={stepComponents} />
+    </div>
+  );
 };
 
 export default StartActivity;
