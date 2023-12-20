5a751fcce47710757a450dd61cc09954555a1da0
elisetelje <teljeelise@gmail.com>
2023-12-20 22:47:16 +0100

    mui input felt og start på logikk

--- src/pages/StartActivity.tsx
+++ src/pages/StartActivity.tsx
@@ -1,5 +1,17 @@
+import TruthOrLie from "../components/TruthOrLie";
+import { auth } from "../firebase/firebase_setup/firebase";
+
 const StartActivity = () => {
-  return <div>Første side på start aktiviteten</div>;
+  //Må sjekke at det finnes en bruker ellerno
+  const user = auth.currentUser;
+  console.log("start activity user", user);
+
+  return (
+    <>
+      <div>Første side på start aktiviteten</div>
+      <TruthOrLie user={user} />
+    </>
+  );
 };
 
 export default StartActivity;
