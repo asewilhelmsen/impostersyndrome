884703c41b92656a1280bf82db0823296ae2890d
amw <ase.wilhelmsen@outlook.com>
2024-01-05 14:00:13 +0100

    Style punktene med bilde og tekst

--- src/components/StartActivity/Expectations.tsx
+++ src/components/StartActivity/Expectations.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/material";
 import A from "../../images/A.svg";
 import B from "../../images/B.svg";
 import C from "../../images/C.svg";
+import Goals from "../Goals";
 
 const Expectations = () => {
   return (
@@ -10,21 +11,59 @@ const Expectations = () => {
         Clarification of Expectations
       </Typography>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
-        <img
-          src={A}
-          alt="Imposter syndrome illustration"
-          style={{ width: "5%" }}
-        ></img>
-        <img
-          src={B}
-          alt="Imposter syndrome illustration"
-          style={{ width: "5%" }}
-        ></img>
-        <img
-          src={C}
-          alt="Imposter syndrome illustration"
-          style={{ width: "5%" }}
-        ></img>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={A}
+            alt="Imposter syndrome illustration"
+            style={{ width: "5%" }}
+          ></img>
+          <Typography>
+            Take turns sharing your strengths and weaknesses in team projects.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={B}
+            alt="Imposter syndrome illustration"
+            style={{ width: "5%" }}
+          ></img>
+          <Typography>
+            Take turns expressing your expectations of the other team members
+            and the team as a whole.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={C}
+            alt="Imposter syndrome illustration"
+            style={{ width: "5%" }}
+          ></img>
+          <Typography>
+            Define and write specific goals regarding teamwork for this sprint:
+          </Typography>
+        </Box>
+        <Goals></Goals>
       </Box>
     </div>
   );
