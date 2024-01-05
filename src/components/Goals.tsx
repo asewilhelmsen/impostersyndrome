45d789f9b4cb584dc6cbdf61e9b24f4025194e8c
amw <ase.wilhelmsen@outlook.com>
2024-01-05 13:59:33 +0100

    legge til flere goals

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -1,5 +1,41 @@
+import React, { useState } from "react";
+import { Box, TextField, Typography, Button } from "@mui/material";
+
 const Goals = () => {
-  return <div>hei</div>;
+  const [goalCount, setGoalCount] = useState([1]);
+
+  const addGoal = () => {
+    setGoalCount((prevGoals) => [...prevGoals, prevGoals.length + 1]);
+  };
+
+  return (
+    <div>
+      {goalCount.map((goalNumber) => (
+        <Box
+          key={goalNumber}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "center",
+          }}
+        >
+          <Typography>{`Goal ${goalNumber}: `}</Typography>
+          <TextField
+            id={`goal${goalNumber}`}
+            variant="outlined"
+            sx={{ width: "30%" }}
+          />
+        </Box>
+      ))}
+      <Button onClick={addGoal} variant="contained" color="primary">
+        Add goal
+      </Button>
+      <Button variant="contained" color="primary">
+        Save goals
+      </Button>
+    </div>
+  );
 };
 
 export default Goals;
