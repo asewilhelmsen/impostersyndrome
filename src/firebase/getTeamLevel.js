d5f88b8ba62282a3857ecae7b59232bc04fbf04f
elisetelje <teljeelise@gmail.com>
2024-01-18 13:05:07 +0100

    level logikk flyttet til Home komponent

---
+++ src/firebase/getTeamLevel.js
@@ -0,0 +1,25 @@
+import { auth, firestore } from "./firebase_setup/firebase";
+import { getDoc, doc } from "@firebase/firestore";
+
+//Returnerer team infoen (level)
+const getTeamLevel = async () => {
+  const teamId = auth.currentUser.uid;
+
+  try {
+    const docRef = doc(firestore, teamId, "teamInfo");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("teamID i get team info", teamId);
+      return docSnap.data().level;
+    } else {
+      //Håndtere feilmelding her
+      console.log("Fant ikke dokument til team info");
+      return 0;
+    }
+  } catch (error) {
+    console.error("Error getting documents: ", error);
+  }
+};
+
+export default getTeamLevel;
