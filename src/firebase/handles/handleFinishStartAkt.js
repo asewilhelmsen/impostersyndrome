6ad639b2cfecfa788971218a303554de214558d6
elisetelje <teljeelise@gmail.com>
2024-01-22 14:30:06 +0100

    lagrer mål når man fullfører startaktiviteten

--- src/firebase/handles/handleFinishStartAkt.js
+++ src/firebase/handles/handleFinishStartAkt.js
@@ -1,18 +1,26 @@
 import { collection, doc, setDoc } from "@firebase/firestore";
 import { firestore, auth } from "../firebase_setup/firebase";
 
-const handleFinishStartAkt = () => {
+const handleFinishStartAkt = (maalData) => {
   const teamId = auth.currentUser?.uid;
   const teamRef = collection(firestore, teamId);
+
+  //Ref steg
   const stegRef = doc(teamRef, "startAktivitetSteg");
+
+  //Ref mål
+  const forventningerRef = doc(teamRef, "forventninger");
+  const maalRef = collection(forventningerRef, "maal");
+  const startAktMaalRef = doc(maalRef, "startAktMaal");
 
   try {
     setDoc(stegRef, {
       steg: -1,
       samtaleSteg: 0,
     });
+    setDoc(startAktMaalRef, maalData);
   } catch (err) {
-    console.log("Kunne ikke oppdatere steg!", err);
+    console.log("Kunne ikke fullføre startaktiviteten!", err);
   }
 };
 
