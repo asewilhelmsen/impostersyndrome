bbcbc4753edd1fc1b7878131533ec04723cd96af
asewilhelmsen <101637785+asewilhelmsen@users.noreply.github.com>
2024-01-31 13:08:25 +0100

    #66 endre styling etter tester (#67)
    * endre farge på badges
    
    * endre siste setning på samtalekort
    
    * fikset warning på IPinfo
    
    * satte alert på innloggingen på toppen av siden
    
    * tooltip på startaktivitet button
    
    * fjerne ikke brukt import
    
    ---------
    
    Co-authored-by: amw <ase.wilhelmsen@outlook.com>

--- src/components/StartAktivitetButton.tsx
+++ src/components/StartAktivitetButton.tsx
@@ -1,9 +1,10 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Tooltip, Typography } from "@mui/material";
 import startActivity from "../images/startActivity.svg";
 import { useNavigate } from "react-router-dom";
 import handleNextStep from "../firebase/handles/handleNextStep";
+import Check from "@mui/icons-material/Check";
 
-const StartAktivitetButton = () => {
+const StartAktivitetButton = ({ level }: { level: number }) => {
   const imageStyle = {
     width: "30%",
   };
@@ -30,9 +31,30 @@ const StartAktivitetButton = () => {
         alt="Button illustration"
         style={imageStyle}
       ></img>
-      <Button variant="contained" onClick={handleClick}>
-        START-AKTIVITET
-      </Button>
+      {level > 0 ? (
+        <Tooltip
+          title={
+            <Typography
+              style={{
+                fontSize: "16px",
+                color: "white",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Check fontSize="medium" /> Start-aktiviteten er allerede fullført
+            </Typography>
+          }
+        >
+          <Button variant="contained" onClick={handleClick}>
+            START-AKTIVITET
+          </Button>
+        </Tooltip>
+      ) : (
+        <Button variant="contained" onClick={handleClick}>
+          START-AKTIVITET
+        </Button>
+      )}
     </div>
   );
 };
