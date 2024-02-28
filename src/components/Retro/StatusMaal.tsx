bc01ea19dcf22aec4981a96f5e6c45a93e25eb2d
amw <ase.wilhelmsen@outlook.com>
2024-02-28 18:08:36 +0100

    midtstille tekst

--- src/components/Retro/StatusMaal.tsx
+++ src/components/Retro/StatusMaal.tsx
@@ -1,30 +1,16 @@
-import { List, ListItem, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { List, ListItem, Typography, Checkbox, Button } from "@mui/material";
 import { useTeamContext } from "../../TeamContext";
-import getMaal from "../../firebase/getMaal";
 import { Maalene } from "../../interfaces";
-import { useEffect, useState } from "react";
-import { collection, doc, onSnapshot } from "@firebase/firestore";
+import { collection, doc, onSnapshot, updateDoc } from "@firebase/firestore";
 import { firestore } from "../../firebase/firebase_setup/firebase";
 
 const StatusMaal = () => {
   const { teamBruker } = useTeamContext();
   const [maalene, setMaalene] = useState<Maalene[]>([]);
-
-  const setMaal = async () => {
-    try {
-      const maalDB = await getMaal();
-      if (maalDB) {
-        const maal: Maalene[] = [];
-        for (let i = 1; i <= Object.keys(maalDB).length; i++) {
-          const key = i.toString();
-          maal.push({ id: key, tekst: maalDB[key] });
-        }
-        setMaalene(maalene);
-      }
-    } catch (error) {
-      console.error("Kan ikke hente målene", error);
-    }
-  };
+  const [checkedItems, setCheckedItems] = useState<{ [key: string]: boolean }>(
+    {}
+  );
 
   useEffect(() => {
     if (teamBruker) {
@@ -42,6 +28,8 @@ const StatusMaal = () => {
             for (let i = 1; i <= Object.keys(data).length; i++) {
               const key = i.toString();
               maalene.push({ id: key, tekst: data[key] });
+              // Initialize checkedItems state
+              setCheckedItems((prev) => ({ ...prev, [key]: false }));
             }
           }
           setMaalene(maalene);
@@ -54,25 +42,33 @@ const StatusMaal = () => {
     }
   }, [teamBruker]);
 
-  /*useEffect(() => {
-    setMaal();
-  }, []);*/
+  const handleCheckboxChange = (id: string) => {
+    setCheckedItems((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
 
   return (
     <>
       <Typography variant="h2">Status av målsetting</Typography>
-      <List sx={{ textAlign: "center" }}>
+      <List>
         {maalene.map((maal, maalIndex) => (
-          <ListItem key={maalIndex} sx={{ paddingLeft: "0" }}>
-            <Typography>
-              <span style={{ fontWeight: "bold" }}>{`Mål ${
-                maalIndex + 1
-              }: `}</span>
+          <ListItem key={maalIndex}>
+            <Checkbox
+              sx={{
+                "& .MuiSvgIcon-root": {
+                  fontSize: 28,
+                  color: "text.primary",
+                },
+              }}
+              checked={checkedItems[maal.id]}
+              onChange={() => handleCheckboxChange(maal.id)}
+            />
+            <Typography sx={{ margin: "auto", marginLeft: 0 }}>
               {maal.tekst}
             </Typography>
           </ListItem>
         ))}
       </List>
+      <Button variant="contained">Lagre</Button>
     </>
   );
 };
